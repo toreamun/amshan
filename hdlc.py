454b054a04fe87d8f21c487ff2179bdee8b87464
tore <tore@amundsen.org>
2018-12-02 01:10:07 +0100

    First working

--- hdlc.py
+++ hdlc.py
@@ -1,4 +1,5 @@
 import fastframecheck
+from array import array
 
 
 class HdlcFrame:
@@ -6,18 +7,144 @@ class HdlcFrame:
     _ffc = None
 
     def __init__(self):
+        self._complete_buffer = bytearray()
         self._buffer = bytearray()
         self._ffc = fastframecheck.FastFrameCheckSequence()
+        self._escape_next = False
+        self._escape_count = 0
+        self.is_header_good = None
+        self.control_position = None
 
     def __len__(self):
         return len(self._buffer)
 
     def append(self, byte):
-        self._buffer.append(byte)
-        self._ffc.update(byte)
-        if self._ffc.is_good:
-            print("Is final")
+        self._complete_buffer.append(byte)
 
+        if self._escape_next:
+            self._escape_next = False
+            unescaped = byte ^ 0x20
+            self._buffer.append(unescaped)
+            self._ffc.update(unescaped)
+        else:
+            if 0x7d == byte:
+                self._escape_next = True
+                self._escape_count += 1
+            else:
+                self._buffer.append(byte)
+                self._ffc.update(byte)
+
+        if self.control_position is None:
+            self.control_position = self._get_control_field_position()
+
+        if self.control_position is not None:
+            if self.is_header_good is None:
+                if len(self._buffer) == self.control_position + 3:
+                    self.is_header_good = self.is_good
+
+
+    @property
+    def escape_next(self):
+        return self._escape_next
+
+    @property
+    def is_good(self):
+        return self._ffc.is_good
+
+    @property
+    def is_checksum_ok(self):
+        fcs_bytes = self._buffer[-2:]
+        if len(fcs_bytes) < 2:
+            print("less")
+        else:
+            fcs = fcs_bytes[1] << 8 | fcs_bytes[0]
+            calculated = self._ffc.compute_checksum(self._buffer, 0, len(self._buffer) - 2)
+            return fcs == calculated
+
+    @property
+    def frame_format(self):
+        if len(self) >= 2:
+            return self._buffer[0] << 8 | self._buffer[1]
+
+    @property
+    def frame_format_type(self):
+        if self.frame_format is not None:
+            return (self.frame_format >> 12) & 0xf
+
+    @property
+    def segmentation(self):
+        if self.frame_format is not None:
+            return ((self.frame_format >> 11) & 0x1) == 0x1
+
+    @property
+    def frame_length(self):
+        if self.frame_format is not None:
+            return self.frame_format & 0x4ff
+
+    @property
+    def destination_address(self):
+        if len(self) >= 2:
+            return self._get_address(2)
+        return None
+
+    @property
+    def source_address(self):
+        destination_adr = self.destination_address
+        if destination_adr is not None:
+            return self._get_address(2 + len(destination_adr))
+        return None
+
+    @property
+    def control(self):
+        if self.control_position is not None and len(self) >= self.control_position:
+            return self._buffer[self.control_position]
+        return None
+
+    @property
+    def header_check_sequence(self):
+        if self.control_position is not None:
+            if len(self._buffer) > self.control_position + 3:
+                return self._buffer[self.control_position + 2] << 8 | self._buffer[self.control_position + 1]
+
+    @property
+    def frame_check_sequence(self):
+        """Frame check sequence if complete frame has been read. None or invalid number if not."""
+        if self.control_position is not None:
+            if len(self._buffer) >= self.control_position + 3:
+                return self._buffer[len(self._buffer) - 1] << 8 | self._buffer[len(self._buffer) - 2]
+
+    @property
+    def information(self):
+        if self.is_good and self.control_position is not None and len(self._buffer) > self.control_position + 3:
+            return self._buffer[self.control_position + 3:-2]
+        return None
+
+    def _get_address(self, position):
+        if len(self) > position:
+            adr = bytearray()
+
+            i = position
+            while True:
+                if i >= len(self):
+                    return None
+
+                current = self._buffer[i]
+                adr.append(current)
+
+                if (current & 0x01) == 0x01:
+                    return adr
+
+                i += 1
+
+        return None
+
+    def _get_control_field_position(self):
+        destination_adr = self.destination_address
+        if destination_adr is not None:
+            source_adr = self.source_address
+            if source_adr is not None:
+                return 2 + len(destination_adr) + len(source_adr)
+        return None
 
 
 class HdlcOctetStuffedFrameReader:
@@ -31,6 +158,8 @@ class HdlcOctetStuffedFrameReader:
 
     def read(self, data):
         self._buffer.extend(data)
+
+        frames = []
 
         if self._frame is None:
             flag_pos = self._buffer.find(self.FLAG_SEQUENCE)
@@ -47,15 +176,62 @@ class HdlcOctetStuffedFrameReader:
         if len(self._buffer) > 0:
             while self._buffer_pos < len(self._buffer):
                 current = self._buffer[self._buffer_pos]
-                print(hex(current))
+                # print(hex(current))
+
+                if(self._frame.is_good):
+                    print("IS good")
 
                 if current == self.FLAG_SEQUENCE[0]:
                     print("FOUND flag seq")
                     if len(self._frame) > 0:
+
+                        if len(self._frame) < 4:
+                            # Frames which are too short (less than 4 octets)
+                            # are silently discarded, and not counted as a FCS error.
+                            print("Too short frame (", len(self._frame)," bytes). Discard: ", self._frame._complete_buffer.hex())
+                        else:
+                            if self._frame.escape_next:
+                                # Frames which end with a Control Escape octet
+                                # followed immediately by a closing Flag Sequence,
+                                # are silently discarded, and not counted as a FCS error.
+                                print("Abort sequence. Discard: ", self._frame._complete_buffer.hex())
+                            else:
+                                actual_length = len(self._frame._buffer)
+                                is_unexpected_length = self._frame.frame_length != actual_length
+
+                                calculated = self._frame.is_checksum_ok
+                                is_unexpected_checksum = calculated != self._frame.is_good
+
+                                if  self._frame.is_good:
+                                    print("### SUCCESS ###")
+                                    if self._frame._escape_count > 0:
+                                        print("ESCAPE success")
+                                else:
+                                    print("Invalid checksum. Frame: ", self._frame._complete_buffer.hex())
+
+                                if is_unexpected_length:
+                                    print("Different lengt, expected = ", self._frame.frame_length, " actual = ",
+                                          actual_length)
+                                else:
+                                    print("Correct length ", actual_length)
+
+                                if is_unexpected_checksum:
+                                    print("Checksum mismatch")
+
+                                if self._frame._escape_count > 0 and self._frame.is_good:
+                                    print("Escaped and good!")
+
+                                if self._frame.segmentation:
+                                    print("Is segmentation")
+
                         print("END of frame")
+                        print()
+
+                        frames.append(self._frame)
+
                         self._frame = HdlcFrame()
                         self._buffer = self._buffer[self._buffer_pos:]
-                        self._buffer_pos = 0
+                        self._buffer_pos = -1
                     else:
                         print("START of frame")
                 else:
@@ -65,3 +241,5 @@ class HdlcOctetStuffedFrameReader:
                     self._frame.append(current)
 
                 self._buffer_pos += 1
+
+        return frames
--- main.py
+++ main.py
@@ -1,21 +1,45 @@
+import fnmatch
+import os
 import socket
 import hdlc
+
+r2 = hdlc.HdlcOctetStuffedFrameReader()
+
+#folder = "/Users/tore/Downloads/han-port-1.15"
+# listOfFiles = os.listdir(folder)
+# pattern = "*.dat"
+# for entry in listOfFiles:
+#     if fnmatch.fnmatch(entry, pattern):
+#         print("####################################")
+#         print(entry)
+#
+#         filepath = os.path.join(folder, entry)
+
+# filepath = "/Users/tore/Downloads/HAN/ikke/han-data-4572.dat"
+# with open(filepath, "rb") as f:
+#     byte = f.read(1)
+#     while byte != b"":
+#         # Do stuff with byte.
+#         r2.read(byte)
+#         byte = f.read(1)
+
 
 # create TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind the socket to the port 23456, and connect
+#bind the socket to the port 23456, and connect
 server_address = ("192.168.1.10", 3001)
 sock.connect(server_address)
 print ("connecting to %s" % (server_address[0]))
 
-r2 = hdlc.HdlcOctetStuffedFrameReader()
-
 while True:
-    b = sock.recv(10)
-    r2.read(b)
+    b = sock.recv(1)
+    frames = r2.read(b)
+
+    for frame in frames:
+        print(frame.information.hex())
 
 
 # close connection
-sock.close()
+#sock.close()
 
