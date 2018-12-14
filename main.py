75547374e8fa6c721868cb634ee89e369ba5e1d1
tore <tore@amundsen.org>
2018-12-14 23:20:11 +0100

    asyncio

--- main.py
+++ main.py
@@ -1,49 +1,30 @@
-import fnmatch
-import os
-import socket
 import hdlc
 import decode
-import json
 import asyncio
 
-reader = hdlc.HdlcOctetStuffedFrameReader()
 
-#folder = "/Users/tore/Downloads/han-port-1.15"
-# listOfFiles = os.listdir(folder)
-# pattern = "*.dat"
-# for entry in listOfFiles:
-#     if fnmatch.fnmatch(entry, pattern):
-#         print("####################################")
-#         print(entry)
-#
-#         filepath = os.path.join(folder, entry)
+async def read_task():
 
-# filepath = "/Users/tore/Downloads/HAN/ikke/han-data-4572.dat"
-# with open(filepath, "rb") as f:
-#     byte = f.read(1)
-#     while byte != b"":
-#         # Do stuff with byte.
-#         r2.read(byte)
-#         byte = f.read(1)
+    socket_reader, socket_writer = await asyncio.open_connection('192.168.1.10', 3001)
+
+    frame_reader = hdlc.HdlcOctetStuffedFrameReader()
+
+    while True:
+        data = await socket_reader.read(1024)
+        frames = frame_reader.read(data)
+
+        for frame in frames:
+            queue.put_nowait(frame)
+
+    # close connection
+    socket_reader.close()
+    socket_writer.close()
 
 
-# create TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#bind the socket to the port 3001, and connect
-server_address = ("192.168.1.10", 3001)
-sock.connect(server_address)
-print ("connecting to %s" % (server_address[0]))
-
-while True:
-    bytes = sock.recv(1)
-    frames = reader.read(bytes)
-
-    for frame in frames:
+async def process_frames():
+    while True:
+        frame = await queue.get()
         msg = decode.LlcPdu.parse(frame.information)
-        #msg = decode.decode_frame(frame._complete_buffer)
-
-        #print(json.dumps(msg))
 
         print(msg)
         print(f"{msg.meter_data.meter_ts}: {msg.meter_data.data.pwr_act_pos} W")
@@ -54,6 +35,15 @@ while True:
         except AttributeError:
             pass
 
-# close connection
-sock.close()
+queue = asyncio.Queue()
 
+loop = asyncio.get_event_loop()
+try:
+    asyncio.ensure_future(read_task())
+    asyncio.ensure_future(process_frames())
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    print("Closing Loop")
+    loop.close()
