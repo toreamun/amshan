31b3b41e6b9638c075369fb7ada1eee824f88be8
tore <tore@amundsen.org>
2020-02-04 23:33:25 +0100

    Added previous_decoder_name property

--- readams/meterdecode/autodecoder.py
+++ readams/meterdecode/autodecoder.py
@@ -5,19 +5,31 @@ from readams.meterdecode import kamstrup
 
 
 class AutoDecoder:
-    _decoder_functions = [aidon.decode_frame, kaifa.decode_frame, kamstrup.decode_frame]
+    _decoder_functions = [
+        ("aidon", aidon.decode_frame),
+        ("kaifa", kaifa.decode_frame),
+        ("kamstrup", kamstrup.decode_frame)]
 
     def __init__(self):
-        self.__previous_success = 0
+        self.__previous_success = None
+
+    @property
+    def previous_decoder_name(self):
+        if self.__previous_success is not None:
+            decoder_name, _ = AutoDecoder._decoder_functions[self.__previous_success]
+            return decoder_name
+        return None
 
     def decode_frame(self, frame: bytes) -> dict:
+        previous_success_index = self.__previous_success if self.__previous_success else 0
+
         for i in range(len(AutoDecoder._decoder_functions)):
-            index = (i + self.__previous_success) % len(AutoDecoder._decoder_functions)
-            decoder = AutoDecoder._decoder_functions[index]
+            index = (i + previous_success_index) % len(AutoDecoder._decoder_functions)
+            _, decoder = AutoDecoder._decoder_functions[index]
             try:
-                decoded = decoder(frame)
+                decoded_frame = decoder(frame)
                 self.__previous_success = index
-                return decoded
+                return decoded_frame
             except construct.ConstructError:
                 pass
 
