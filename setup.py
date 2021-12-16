61695da124419a2fb06a2860fee1d8f149d23200
Tore Amundsen <tore@amundsen.org>
2021-12-16 22:58:55 +0100

    Update setup.py
    Back to semver

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
     name="amshan",
-    version="2021.12.1",
+    version="1.0.0",
     author="Tore Amundsen",
     author_email="tore@amundsen.org",
     description="Decode MBUS (Meter Bus) data with special support for norwegian AMS-smart meters (HAN port)",
