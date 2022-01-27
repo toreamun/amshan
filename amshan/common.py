84fcae661c989015f0c8d284ab24067944b8136d
tore <tore@amundsen.org>
2022-01-27 23:28:34 +0100

    wrong attribute

--- amshan/common.py
+++ amshan/common.py
@@ -1,7 +1,7 @@
 """Common types use dby other modules."""
 from __future__ import annotations
 
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
 T = TypeVar("T")  # pylint: disable=invalid-name
@@ -10,7 +10,7 @@ T = TypeVar("T")  # pylint: disable=invalid-name
 class MeterReaderBase(ABC, Generic[T]):
     """Abstract base class for meter reader types."""
 
-    @property.getter
+    @property
     @abstractmethod
     def is_in_hunt_mode(self) -> bool:
         """Return True when reader is hunting for start of message."""
