d58c25ce63920cac25283923b80fa0d883506fc4
tore <tore@amundsen.org>
2022-02-04 23:35:40 +0100

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
     name="amshan",
-    version="2.0.0",
+    version="2.0.1",
     author="Tore Amundsen",
     author_email="tore@amundsen.org",
     description="Decode P1 and MBUS (Meter Bus) DLMS data. Special support for norwegian and swedish meters.",
